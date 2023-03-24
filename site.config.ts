a146c74a895ad08f6b21601c209700ee15e48f92
yangdong <don.yangdong@gmail.com>
2023-03-24 18:26:29 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'don.yangdong@gmail.com',
+  twitter: null,
   github: 'yangdong',
-  linkedin: 'fisch2',
+  linkedin: 'young-evan',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
